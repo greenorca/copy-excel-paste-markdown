ec7e493e0a70e9faa3eb4b8f666245cb2f214321
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-04-12 10:28:58 +0200

    reworked to fit ods and html clipbords with firefox

--- script.js
+++ script.js
@@ -20,62 +20,39 @@ editor.addEventListener("paste", function(event) {
     return
   }
 
-  var rows = data.split((/[\u0085\u2028\u2029]|\r\n?/g)).map(function(row) {
-    row = row.replace('\n', ' ')
-    return row.split("\t")
-  })
-
-  var colAlignments = []
-
-  var columnWidths = rows[0].map(function(column, columnIndex) {
-    var alignment = "l"
-    var re = /^(\^[lcr])/i
-    var m = column.match(re)
-    if (m) {
-        var align = m[1][1].toLowerCase()
-        if (align === "c") {
-          alignment = "c"
-        } else if (align === "r") {
-          alignment = "r"
+  //var rows = data.split((/[\u0085\u2028\u2029]|\r\n?/g)).map(function(row) {
+  var rows = data.split((/\n/g))
+  
+  var markdownRows = []
+  
+  nCells = 0;
+  
+  rows.forEach( (row,index) => {
+        
+    line = "| "
+    
+    cells = row.split("\t")
+    cells.forEach( cell => {
+      if (index == 0){
+        line += " **"+ cell.trim() + "** |"
+      } else { 
+        line += cell + " | "
         }
-      }
-    colAlignments.push(alignment)
-    column = column.replace(re, "")
-    rows[0][columnIndex] = column
-    return columnWidth(rows, columnIndex)
-  })
-  var markdownRows = rows.map(function(row, rowIndex) {
-    // | Name         | Title | Email Address  |
-    // |--------------|-------|----------------|
-    // | Jane Atler   | CEO   | jane@acme.com  |
-    // | John Doherty | CTO   | john@acme.com  |
-    // | Sally Smith  | CFO   | sally@acme.com |
-    return "| " + row.map(function(column, index) {
-      return column + Array(columnWidths[index] - column.length + 1).join(" ")
-    }).join(" | ") + " |"
-    row.map
-
-  })
-  markdownRows.splice(1, 0, "|" + columnWidths.map(function(width, index) {
-    var prefix = ""
-    var postfix = ""
-    var adjust = 0
-    var alignment = colAlignments[index]
-    if (alignment === "r") {
-      postfix = ":"
-      adjust = 1
-    } else if (alignment == "c") {
-      prefix = ":"
-      postfix = ":"
-      adjust = 2
+    })
+    nCells = Math.max(nCells, cells.length)
+    
+    markdownRows.push(line)
+    
+    if (index == 0){
+      line = "|"
+      for ( i=0; i < nCells;i++){
+        line += "---|"  
+      }  
+      markdownRows.push(line)
     }
-    return prefix + Array(columnWidths[index] + 3 - adjust).join("-") + postfix
-  }).join("|") + "|")
-
-  // https://www.w3.org/TR/clipboard-apis/#the-paste-action
-  // When pasting, the drag data store mode flag is read-only, hence calling
-  // setData() from a paste event handler will not modify the data that is
-  // inserted, and not modify the data on the clipboard.
+    
+  })
+  
 
   event.target.value = markdownRows.join("\n")
   return false
